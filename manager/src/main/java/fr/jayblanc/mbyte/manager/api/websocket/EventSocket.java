6bab138a8274c3b23975ea1965164edf4f8810e5
Jérôme Blanchard <jblanchard@jahia.com>
2026-01-26 23:06:03 +0100

    fix: event socket filtering.

--- manager/src/main/java/fr/jayblanc/mbyte/manager/api/websocket/EventSocket.java
+++ manager/src/main/java/fr/jayblanc/mbyte/manager/api/websocket/EventSocket.java
@@ -23,10 +23,14 @@ import fr.jayblanc.mbyte.manager.notification.entity.Event;
 import io.quarkus.security.Authenticated;
 import io.quarkus.vertx.ConsumeEvent;
 import io.quarkus.websockets.next.OnOpen;
+import io.quarkus.websockets.next.OpenConnections;
 import io.quarkus.websockets.next.WebSocket;
 import io.quarkus.websockets.next.WebSocketConnection;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -36,17 +40,20 @@ import java.util.logging.Logger;
 
 @Authenticated
 @WebSocket(path = "/notifications")
+@ApplicationScoped
 public class EventSocket {
 
     private static final Logger LOGGER = Logger.getLogger(EventSocket.class.getName());
+    private final Map<String, String> ownerConnections = new ConcurrentHashMap<>();
 
-    @Inject WebSocketConnection connection;
+    @Inject OpenConnections connections;
     @Inject AuthenticationService auth;
     @Inject ObjectMapper objectMapper;
 
     @OnOpen
-    public void onOpen() {
-        LOGGER.log(Level.INFO, "Opening event socket {0}", this);
+    public void onOpen(WebSocketConnection connection) {
+        LOGGER.log(Level.INFO, "Opening event socket for owner {0}", auth.getConnectedIdentifier());
+        this.ownerConnections.put(auth.getConnectedIdentifier(), connection.id());
         Event event = Event.build(auth.getConnectedIdentifier(), "WS_CONNECTED", auth.getConnectedIdentifier(), "Welcome onboard " + auth.getConnectedProfile().getFullname() + "!");
         try {
             connection.sendTextAndAwait(objectMapper.writeValueAsString(event));
@@ -57,14 +64,24 @@ public class EventSocket {
 
     @ConsumeEvent(value = NotificationService.NOTIFICATION_TOPIC, blocking = true)
     public void onMessage(Event event) {
-        LOGGER.log(Level.INFO, "Received event, checking connection owner {0} against event owner {1}", new Object[]{auth.getConnectedIdentifier(), event.getOwner()});
-        if (event.getOwner().equals(auth.getConnectedIdentifier())) {
-            LOGGER.log(Level.INFO, "Sending event to websocket connection {0}", connection);
+        LOGGER.log(Level.INFO, "Received event, searching connection for event owner {0}", event.getOwner());
+        if (event.getOwner() == null || event.getOwner().isEmpty()) {
+            for (WebSocketConnection connection : connections) {
+                LOGGER.log(Level.INFO, "Broadcasting event to connection {0}", connection);
+                try {
+                    connection.sendTextAndAwait(objectMapper.writeValueAsString(event));
+                } catch (JsonProcessingException e) {
+                    LOGGER.log(Level.SEVERE, "Failed to serialize event to JSON", e);
+                }
+            }
+        }
+        connections.findByConnectionId(ownerConnections.get(event.getOwner()) ).ifPresent(connection -> {
+            LOGGER.log(Level.INFO, "Found websocket connection {0} for owner {1}", new Object[]{connection, event.getOwner()});
             try {
                 connection.sendTextAndAwait(objectMapper.writeValueAsString(event));
             } catch (JsonProcessingException e) {
                 LOGGER.log(Level.SEVERE, "Failed to serialize event to JSON", e);
             }
-        }
+        });
     }
 }
